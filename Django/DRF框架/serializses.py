e24e7ec47b1b590bebe23f365cc85eb5fb9a78b4
Aloof-0 <935722505@qq.com>
2021-01-02 14:19:03 +0800

    cold

---
+++ Django/DRF框架/serializses.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/1/2 14:16
+@Auth ： 高冷Aloof
+@File ：serializses.py
+@IDE ：PyCharm
+@Motto：ABC(Always Be Coding)
+"""
+# 针对BookInfo模块类对象,定义一个序列化器,这个序列化专门用来操作BookInfo 对象
+# (1). 明确操作的目标数据  -- 模型类
+# (2). 继承自serializer
+# (3). 定义和模型类"同名"类属性, 类型 --- 对应的形式. 来确定参与序列号模型类属性
+# (4).
+#  serializers模块
+from rest_framework import serializers
+
+
+class BookInfoserializer(serializers.Serializer):
+    btitle = serializers.CharField(max_length=20)
+    bpub_date = serializers.DateField()
+    bread = serializers.IntegerField()
+    bcomment = serializers.IntegerField()
+    is_delete = serializers.BooleanField()
+    image = serializers.ImageField()
+    # 1.序列化为关联对象的主键值,read_only=True表明此属性/字段只参与序列化操作
+    book_pk = serializers.PrimaryKeyRelatedField(read_only=True)
+    # 2.序列化为关联对象的__str__方法返回的结果
+    book_str = serializers.StringRelatedField()
+
+
+
+
+
